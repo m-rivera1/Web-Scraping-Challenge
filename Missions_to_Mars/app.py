75f82ffeb3cf38e2a75e34e0be060ec206edb817
m-rivera1 <mike.11.mxr@gmail.com>
2021-01-11 16:45:59 -0600

    fixed type on scrape function

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -27,7 +27,7 @@ def home():
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape()
+    mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
